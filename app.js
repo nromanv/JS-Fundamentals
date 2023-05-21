ed7335a40aab2935d464e16e0af2eb0423647c8c
nromanv <nakonechnyj.roman@gmail.com>
2023-05-21 15:08:25 +0300

    ht update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// nw1.2
+// task-2
 console.log("Nakonechnyi");
 
-// hw1.3
+// task-3
 let carName;
 let carModel;
 
@@ -13,7 +13,7 @@ console.log(carName, carModel);
 carName = carModel;
 console.log(carName, carModel);
 
-// hw1.4
+// task-4
 let student = {
     name: "roman",
     age: 38,
@@ -22,12 +22,12 @@ let student = {
     newInfo: undefined
 }; 
 
-// hw1.5
+// task-5
 let isAdult;
 isAdult = confirm("Are you adult?");
 console.log(isAdult);
 
-// hw1.6
+// task-6
 let name = "roman";
 let surname = "nakonechnyi";
 let group = 1;
@@ -46,12 +46,12 @@ let otherInfo = undefined;
 console.log(info);
 console.log(otherInfo);
 
-// hw1.7
+// task-7
 let userLogin = prompt("What is your login?");
 let userEmail = prompt("What is your email?");
 let userPassword = prompt("What is your password?");
 
 alert(`Dear User, your login is ${userLogin}, your email is ${userEmail}, your password is ${userPassword}`);
 
-//hw1.8
+//task-8
 
