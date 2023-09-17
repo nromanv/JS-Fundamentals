784c4e34a70f81132e66fdfab69dd6f89f77f32e
nromanv <nakonechnyj.roman@gmail.com>
2023-09-17 17:05:12 +0300

    update task-2

---
+++ homework-9/hometask-9.js
@@ -0,0 +1,18 @@
+//-----------------task-1---------------------------
+/*
+const $greenHead = $('.head').css("background", "green");
+const $innerhead = $('.inner').css("fontSize", "35px");
+*/
+
+//-----------------task-2---------------------------
+/*
+const $link = ($('.link').text());
+const regExp = /^http:\/\//
+
+if (regExp.test($link)) {
+    $('.link').attr('target', '_blank');
+}
+console.log($('.link')[0].outerHTML);
+*/
+
+//-----------------task-2---------------------------
